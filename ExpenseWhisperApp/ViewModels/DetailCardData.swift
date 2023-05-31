8df510a023f26bee03e13cf47c621ceb74df997b
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-05-31 17:12:59 +0900

    add : DetailCardを作成

--- ExpenseWhisperApp/ViewModels/DetailCardData.swift
+++ ExpenseWhisperApp/ViewModels/DetailCardData.swift
@@ -9,4 +9,30 @@ import Foundation
 
 class DetailCardData: ObservableObject {
     
+    @Published var costs: [UsedCost] = [
+        UsedCost(usedCost: 0, category: CostCategory.food),
+        UsedCost(usedCost: 0, category: CostCategory.transportation),
+        UsedCost(usedCost: 0, category: CostCategory.entertainment),
+        UsedCost(usedCost: 0, category: CostCategory.medicalAndCosme),
+        UsedCost(usedCost: 0, category: CostCategory.livingware),
+        UsedCost(usedCost: 0, category: CostCategory.others)
+    ]
+    
+    func plusCost(cost: Cost) {
+        switch cost.category {
+        case .food:
+            self.costs[0].usedCost += cost.price
+        case .transportation:
+            self.costs[1].usedCost += cost.price
+        case .entertainment:
+            self.costs[2].usedCost += cost.price
+        case .medicalAndCosme:
+            self.costs[3].usedCost += cost.price
+        case .livingware:
+            self.costs[4].usedCost += cost.price
+        case .others:
+            self.costs[5].usedCost += cost.price
+        }
+    }
+    
 }
