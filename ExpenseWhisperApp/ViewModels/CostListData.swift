2099d3ec51e42791f0374ddfdf77e76c70e5a872
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-05-31 17:16:36 +0900

    add : Listに表示するためのDataを作成

--- ExpenseWhisperApp/ViewModels/CostListData.swift
+++ ExpenseWhisperApp/ViewModels/CostListData.swift
@@ -8,5 +8,19 @@
 import Foundation
 
 class CostListData: ObservableObject {
+    @Published var costList: [Cost] = []
+    @Published var isAddView = false
     
+    func delete(offset: IndexSet) {
+        costList.remove(atOffsets: offset)
+    }
+    
+    func didTapSaveButton(cost: Cost) {
+        costList.append(cost)
+        isAddView = false
+    }
+    
+    func didTapCancelButton() {
+        isAddView = false
+    }
 }
