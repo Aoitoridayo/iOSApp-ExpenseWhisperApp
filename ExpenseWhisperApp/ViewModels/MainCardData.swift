38c90fe4056831c7188389f3fc97b2bdc36e4a12
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-05-31 17:09:13 +0900

    add : MainCardDataを作成

--- ExpenseWhisperApp/ViewModels/MainCardData.swift
+++ ExpenseWhisperApp/ViewModels/MainCardData.swift
@@ -8,5 +8,39 @@
 import Foundation
 
 class MainCardData: ObservableObject {
+    static var used: Int = 0
+    static var goal: Int = 50000
     
+    @Published var goalMoney: Int = 0
+    @Published var usedMoney: Int = 0
+    @Published var available: Int = 0
+    @Published var isFlipped = false
+    
+    func updataGoal(goal: Int) {
+        Self.goal = goal
+    }
+    
+    func updataUsed(price: Int) {
+        Self.used += price
+        updataCard()
+    }
+    
+    func clearUsed() {
+        Self.used = 0
+        updataCard()
+    }
+    
+    func onAppear() {
+        updataCard()
+    }
+    
+    private func updataCard() {
+        goalMoney = Self.goal
+        usedMoney = Self.used
+        available = Self.goal - Self.used
+    }
+    
+    func didTapCard() {
+        isFlipped.toggle()
+    }
 }
