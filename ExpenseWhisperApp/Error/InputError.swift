25126a4172191c22dd5e2a1c7d7ab19cd055d3ec
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-05-31 17:03:46 +0900

    add : 入力エラーを定義

--- ExpenseWhisperApp/Error/InputError.swift
+++ ExpenseWhisperApp/Error/InputError.swift
@@ -8,5 +8,19 @@
 import Foundation
 
 enum InputError: Error {
+    case emptyTitle
+    case wrongPrice
+    case emptyCategory
     
+    var title: String {
+        switch self {
+            
+        case .emptyTitle:
+            return "タイトルを入力してください"
+        case .wrongPrice:
+            return "正しい金額を入力してください"
+        case .emptyCategory:
+            return "カテゴリーを選択してください"
+        }
+    }
 }
